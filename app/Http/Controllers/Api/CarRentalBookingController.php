3bb36698afb04f9dbdde5dc34787c455cc1886b8
shabbirislam <shabbirislam801@gmail.com>
2023-03-14 20:19:53 +0100

    added CarRentalBookingController.php and change the welcome page

---
+++ app/Http/Controllers/Api/CarRentalBookingController.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CarRentalBookingController extends Controller
+{
+    protected $bookings;
+
+    /*public function __construct(bookings $bookings){
+        $this->bookings = $bookings;
+    }*/
+
+    /**
+     * Create bookings
+     * @OA\Post (
+     *     path="/bookings-rental/api/v1/bookings/store",
+     *     tags={"bookings"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="title",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="content",
+     *                          type="string"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "title":"example title",
+     *                     "content":"example content"
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="title", type="string", example="title"),
+     *              @OA\Property(property="content", type="string", example="content"),
+     *              @OA\Property(property="updated_at", type="string", example="2021-12-11T09:25:53.000000Z"),
+     *              @OA\Property(property="created_at", type="string", example="2021-12-11T09:25:53.000000Z"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="invalid",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="msg", type="string", example="fail"),
+     *          )
+     *      )
+     * )
+     */
+    public function store(Request $request){
+        //$bookings = $this->bookings->createbookings($request->all());
+        $data = [
+            'status' => 'success',
+            'message' => 'Hello World!'
+        ];
+        return response()->json($data);
+    }
+
+    /**
+     * Update bookings
+     * @OA\Put (
+     *     path="/bookings-rental/api/v1/bookings/update/{id}",
+     *     tags={"bookings"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="title",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="content",
+     *                          type="string"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "title":"example title",
+     *                     "content":"example content"
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="title", type="string", example="title"),
+     *              @OA\Property(property="content", type="string", example="content"),
+     *              @OA\Property(property="updated_at", type="string", example="2021-12-11T09:25:53.000000Z"),
+     *              @OA\Property(property="created_at", type="string", example="2021-12-11T09:25:53.000000Z")
+     *          )
+     *      )
+     * )
+     */
+    public function update($id, Request $request){
+        try {
+            $bookings = $this->bookings->updatebookings($id,$request->all());
+            return response()->json($bookings);
+        }catch (ModelNotFoundException $exception){
+            return response()->json(["msg"=>$exception->getMessage()],404);
+        }
+    }
+
+    /**
+     * Get Detail bookings
+     * @OA\Get (
+     *     path="/bookings-rental/api/v1/bookings/get/{id}",
+     *     tags={"bookings"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="title", type="string", example="title"),
+     *              @OA\Property(property="content", type="string", example="content"),
+     *              @OA\Property(property="updated_at", type="string", example="2021-12-11T09:25:53.000000Z"),
+     *              @OA\Property(property="created_at", type="string", example="2021-12-11T09:25:53.000000Z")
+     *         )
+     *     )
+     * )
+     */
+    public function get($id){
+        $bookings = $this->bookings->getbookings($id);
+        if($bookings){
+            return response()->json($bookings);
+        }
+        return response()->json(["msg"=>"bookings item not found"],404);
+    }
+
+    /**
+     * Get List bookings
+     * @OA\Get (
+     *     path="/bookings-rental/api/v1/bookings/gets",
+     *     tags={"bookings"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="rows",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="_id",
+     *                         type="number",
+     *                         example="1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="title",
+     *                         type="string",
+     *                         example="example title"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="content",
+     *                         type="string",
+     *                         example="example content"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="updated_at",
+     *                         type="string",
+     *                         example="2021-12-11T09:25:53.000000Z"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="created_at",
+     *                         type="string",
+     *                         example="2021-12-11T09:25:53.000000Z"
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function gets(){
+        $bookingss = $this->bookings->getsbookings();
+        return response()->json(["rows"=>$bookingss]);
+    }
+
+    /**
+     * Delete bookings
+     * @OA\Delete (
+     *     path="/bookings-rental/api/v1/bookings/delete/{id}",
+     *     tags={"bookings"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="msg", type="string", example="delete bookings success")
+     *         )
+     *     )
+     * )
+     */
+    public function delete($id){
+        try {
+            $bookings = $this->bookings->deletebookings($id);
+            return response()->json(["msg"=>"delete bookings success"]);
+        }catch (ModelNotFoundException $exception){
+            return response()->json(["msg"=>$exception->getMessage()],404);
+        }
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,13 @@
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-
+            <?php
+            if(DB::connection()->getPdo())
+            {
+                echo "Successfully connected to the database => "
+                    .DB::connection()->getDatabaseName();
+            }
+            ?>
         </div>
     </div>
 </div>
